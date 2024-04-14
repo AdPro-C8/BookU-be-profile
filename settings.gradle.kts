ceff6319691bb600754132f293614a6ea9b274d6
CyberSleeper <mahartha.gemilang@gmail.com>
2024-04-15 01:01:06 +0700

    Initial Commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "BookU-be-profile"
---
+++ src/main/java/id/ac/ui/cs/advprog/bookubeprofile/BookUBeProfileApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.bookubeprofile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookUBeProfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookUBeProfileApplication.class, args);
+	}
+
+}
