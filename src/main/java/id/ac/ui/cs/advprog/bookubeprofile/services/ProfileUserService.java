76cd0ce569a2696c0c99cc4d3e050880caccdc60
CyberSleeper <mahartha.gemilang@gmail.com>
2024-04-17 18:08:53 +0700

    add(services): Profile User Service

---
+++ src/main/java/id/ac/ui/cs/advprog/bookubeprofile/services/ProfileUserService.java
@@ -0,0 +1,24 @@
+package id.ac.ui.cs.advprog.bookubeprofile.services;
+
+import id.ac.ui.cs.advprog.bookubeprofile.models.ProfileUser;
+import id.ac.ui.cs.advprog.bookubeprofile.repositories.ProfileUserRepository;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ProfileUserService {
+    private final ProfileUserRepository profileUserRepository;
+
+    public ProfileUserService(ProfileUserRepository profileUserRepository) {
+        this.profileUserRepository = profileUserRepository;
+    }
+
+    public List<ProfileUser> allUsers() {
+        List<ProfileUser> users = new ArrayList<>();
+        profileUserRepository.findAll().forEach(users::add);
+        return users;
+    }
+}
