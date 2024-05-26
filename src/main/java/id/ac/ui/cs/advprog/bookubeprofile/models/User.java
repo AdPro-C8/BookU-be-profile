4d82aa03c96de114aa7d2db93a3b8ceb81d722da
CyberSleeper <mahartha.gemilang@gmail.com>
2024-05-26 21:46:00 +0700

    add(model): user

---
+++ src/main/java/id/ac/ui/cs/advprog/bookubeprofile/models/User.java
@@ -0,0 +1,53 @@
+package id.ac.ui.cs.advprog.bookubeprofile.models;
+
+import java.util.Collection;
+import java.util.UUID;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @Builder
+@NoArgsConstructor @AllArgsConstructor
+public class User implements UserDetails {
+    private UUID id;
+    private String username;
+    private String password;
+    private String email;
+    private String role;
+    private Collection<? extends GrantedAuthority> authorities;
+    private boolean accountNonExpired;
+    private boolean accountNonLocked;
+    private boolean credentialsNonExpired;
+    private boolean enabled;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return accountNonExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return accountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return credentialsNonExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+}
